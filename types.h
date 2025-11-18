0e25004fac54cc57f7e9f302aa78c106693bfd9b
Srisanthosh <123087875+srisanthosh-babu@users.noreply.github.com>
2025-11-18 09:29:59 +0530

    Required data structures and status varibales
    This file defines all essential custom data types and structures used across the APC project for consistent, modular development. It centralizes type definitions to improve code readability, maintainability, and reduce duplication across modules.

---
+++ types.h
@@ -0,0 +1,13 @@
+#ifndef TYPES_H
+#define TYPES_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+typedef enum{
+	SUCCESS,FAILURE,LIST_EMPTY
+}Status;
+#endif
+
